563ca5db4173674dc3aafbba5ffd56d78ada2f2c
Nyakeh Rogers <nyakeh123@gmail.com>
2015-10-18 17:05:22 +0100

    Implemented template navigation drawer. Using a single activity to control all page switching between activities.

--- app/src/main/java/uk/co/nyakeh/papertrail/SingleFragmentActivity.java
+++ app/src/main/java/uk/co/nyakeh/papertrail/SingleFragmentActivity.java
@@ -3,23 +3,21 @@ package uk.co.nyakeh.papertrail;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-public abstract class SingleFragmentActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends BaseActivity {
 
     protected abstract android.support.v4.app.Fragment createFragment();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        Fragment fragment = fragmentManager.findFragmentById(R.id.content_container);
         if (fragment == null) {
             fragment = createFragment();
             fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
+                    .add(R.id.content_container, fragment)
                     .commit();
         }
     }
