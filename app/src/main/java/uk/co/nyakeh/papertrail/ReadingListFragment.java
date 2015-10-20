cfff0fd3c2e0e7b83efb3a5e6511f02db5cc03cf
Nyakeh Rogers <nyakeh123@gmail.com>
2015-10-20 21:54:48 +0100

    Gave reading list items material update.

--- app/src/main/java/uk/co/nyakeh/papertrail/ReadingListFragment.java
+++ app/src/main/java/uk/co/nyakeh/papertrail/ReadingListFragment.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class ReadingListFragment extends Fragment {
     private RecyclerView mBookRecyclerView;
-    private ArchivedBookAdapter mArchivedBookAdapter;
+    private ReadingListBookAdapter mArchivedBookAdapter;
     private TextView mBookListEmptyMessageView;
 
     @Override
@@ -35,9 +35,6 @@ public class ReadingListFragment extends Fragment {
         mBookListEmptyMessageView = (TextView) view.findViewById(R.id.book_list_empty_message);
         mBookRecyclerView = (RecyclerView) view.findViewById(R.id.book_recycler_view);
         mBookRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST);
-        mBookRecyclerView.addItemDecoration(itemDecoration);
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +60,7 @@ public class ReadingListFragment extends Fragment {
         List<Book> books = bookLab.getReadingList();
 
         if (mArchivedBookAdapter == null) {
-            mArchivedBookAdapter = new ArchivedBookAdapter(books);
+            mArchivedBookAdapter = new ReadingListBookAdapter(books);
             mBookRecyclerView.setAdapter(mArchivedBookAdapter);
         } else {
             mArchivedBookAdapter.setBooks(books);
@@ -85,39 +82,24 @@ public class ReadingListFragment extends Fragment {
         updateUI();
     }
 
-    private class ArchivedBookHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private class ReadingListBookHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private Book mBook;
         private TextView mTitleTextView;
         private TextView mAuthorTextView;
-        private TextView mDateFinishedTextView;
-        private ImageView mImageView;
 
-        public ArchivedBookHolder(View itemView) {
+        public ReadingListBookHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_book_title);
             mAuthorTextView = (TextView) itemView.findViewById(R.id.list_item_book_author);
-            mDateFinishedTextView = (TextView) itemView.findViewById(R.id.list_item_book_date_finished);
-            mImageView = (ImageView) itemView.findViewById(R.id.list_item_book_image);
         }
 
         private void bindBook(Book book) {
             mBook = book;
             mTitleTextView.setText(book.getTitle());
             mAuthorTextView.setText(book.getAuthor());
-            String formattedFinishedDate = DateFormat.format("EEEE, MMM dd, yyyy", mBook.getDateFinished()).toString();
-            mDateFinishedTextView.setText(formattedFinishedDate);
-
-            String safePicassoImageUrl = (book.getImageUrl().isEmpty()) ? "fail_gracefully_pls" : book.getImageUrl();
-            Picasso.with(getActivity())
-                    .load(safePicassoImageUrl)
-                    .placeholder(R.drawable.books)
-                    .error(R.drawable.books)
-                    .resize(90, 90)
-                    .centerCrop()
-                    .into(mImageView);
         }
 
         @Override
@@ -127,23 +109,23 @@ public class ReadingListFragment extends Fragment {
         }
     }
 
-    private class ArchivedBookAdapter extends RecyclerView.Adapter<ArchivedBookHolder> {
+    private class ReadingListBookAdapter extends RecyclerView.Adapter<ReadingListBookHolder> {
 
         private List<Book> mBooks;
 
-        public ArchivedBookAdapter(List<Book> books) {
+        public ReadingListBookAdapter(List<Book> books) {
             mBooks = books;
         }
 
         @Override
-        public ArchivedBookHolder onCreateViewHolder(ViewGroup parent, int i) {
+        public ReadingListBookHolder onCreateViewHolder(ViewGroup parent, int i) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(R.layout.list_item_book_archived, parent, false);
-            return new ArchivedBookHolder(view);
+            View view = layoutInflater.inflate(R.layout.list_item_book_reading_list, parent, false);
+            return new ReadingListBookHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(ArchivedBookHolder archivedBookHolder, int position) {
+        public void onBindViewHolder(ReadingListBookHolder archivedBookHolder, int position) {
             Book book = mBooks.get(position);
             archivedBookHolder.bindBook(book);
         }
