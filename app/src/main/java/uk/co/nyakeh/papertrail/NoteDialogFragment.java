d8e14191fd7ab6666ed170e51d9a9c6e8ee3fae6
Nyakeh Rogers <nyakeh123@gmail.com>
2015-11-02 00:14:56 +0000

    Implemented dismiss button (need to make it update the note in the db). Highlight the note title in the modal view.

--- app/src/main/java/uk/co/nyakeh/papertrail/NoteDialogFragment.java
+++ app/src/main/java/uk/co/nyakeh/papertrail/NoteDialogFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class NoteDialogFragment extends DialogFragment {
 
@@ -12,6 +13,23 @@ public class NoteDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.dialog_note, container, false);
         getDialog().setTitle("Note Tings");
+
+        Button dismiss = (Button) rootView.findViewById(R.id.dialog_note_delete);
+        dismiss.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        //TODO  updateNote()
     }
 }
