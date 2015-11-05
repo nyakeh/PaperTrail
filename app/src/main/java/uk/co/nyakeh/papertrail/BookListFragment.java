ebc65f6e5e0b6da582b6b6223ddc7c05f01f75b5
Nyakeh Rogers <nyakeh123@gmail.com>
2015-11-05 19:12:13 +0000

    Added intro animation to currently reading list items.

--- app/src/main/java/uk/co/nyakeh/papertrail/BookListFragment.java
+++ app/src/main/java/uk/co/nyakeh/papertrail/BookListFragment.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -135,9 +137,7 @@ public class BookListFragment extends Fragment {
     }
 
     public class BookAdapter extends RecyclerView.Adapter<BookHolder> {
-
         private List<Book> mBooks;
-
         public BookAdapter(List<Book> books) {
             mBooks = books;
         }
