6b2824ea0d43a17f807d7b2996c027b6d1f20081
Nyakeh Rogers <nyakeh123@gmail.com>
2015-11-12 23:20:42 +0000

    Created new activity to host google books api book search to add new book.

---
+++ app/src/main/java/uk/co/nyakeh/papertrail/SearchActivity.java
@@ -0,0 +1,22 @@
+package uk.co.nyakeh.papertrail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SearchActivity extends AppCompatActivity {
+    private EditText mSearchText;
+    private Button mSearchClearText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
